08add3563f6abb149bac1ee8ab5bd15fd4b51123
lordcodes <83734728+lrdcxdes@users.noreply.github.com>
2024-02-04 14:02:10 +0200

    a

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 call ".\npm_install.bat"
 
 :_minerstart
-node send_universal_TESTING.js --api tonhub --timeout 1 --givers 100 --bin opencl
+node send_universal.js --api tonhub --timeout 5 --givers 1000 --bin opencl
 goto _minerstart
 
 pause
